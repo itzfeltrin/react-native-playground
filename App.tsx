846d5423592fc27bf4312fb8f23efafcab6eec9c
itzfeltrin <pedrofeltrin45@yahoo.com>
2021-04-05 22:49:11 -0300

    feat: initial config

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// screens
+import {HomeScreen} from './src/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => {
+    return <HomeScreen />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = (): JSX.Element => {
+    return (
+        <View>
+            <Text>JAJAJA</Text>
+        </View>
+    );
+};
+
+export default Home;
