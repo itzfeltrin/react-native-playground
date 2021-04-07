22eb52775b7cbd047ae4ccdd78e155a730a16203
Pedro Feltrin <pedrofeltrin45@yahoo.com>
2021-04-06 21:25:58 -0300

    feat: add pinch gesture handler (not working properly)

--- src/screens/Draggable/Draggable.tsx
+++ src/screens/Draggable/Draggable.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
-import {PanGestureHandler} from 'react-native-gesture-handler';
-import {useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withDecay} from 'react-native-reanimated';
+import React, {useCallback} from 'react';
+import {
+    PanGestureHandler,
+    PanGestureHandlerGestureEvent,
+    PinchGestureHandler,
+    PinchGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+    useAnimatedGestureHandler,
+    useAnimatedStyle,
+    useSharedValue,
+    withDecay,
+} from 'react-native-reanimated';
 import {withBouncing} from 'react-native-redash';
 // components
 import {Container} from '../../common';
@@ -10,8 +20,9 @@ import {Box, DRAGGABLE_SIZE} from './styles';
 const DraggableScreen = (): JSX.Element => {
     const x = useSharedValue<number>(0);
     const y = useSharedValue<number>(0);
+    const scale = useSharedValue<number>(1);
 
-    const handleDrag = useAnimatedGestureHandler({
+    const handleDrag = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
         onStart: (_, ctx) => {
             ctx.x = x.value;
             ctx.y = y.value;
@@ -33,20 +44,39 @@ const DraggableScreen = (): JSX.Element => {
                     velocity: event.velocityY,
                 }),
                 0,
-                SCREEN_HEIGHT - DRAGGABLE_SIZE - 74,
+                SCREEN_HEIGHT - DRAGGABLE_SIZE - 74, // 74 is the height of android's action bar (approximately)
             );
+        },
+    });
+
+    // const handlePinch = useCallback(
+    //     (event: PinchGestureHandlerGestureEvent) => {
+    //         const {nativeEvent} = event;
+    //         const value = Number(nativeEvent.scale.toFixed(2));
+    //         scale.value = value;
+    //     },
+    //     [scale],
+    // );
+
+    const handlePinch = useAnimatedGestureHandler<PinchGestureHandlerGestureEvent>({
+        onStart: (evt, ctx) => {
+            console.log(evt, ctx);
         },
     });
 
     // animated style
     const boxAnimatedStyle = useAnimatedStyle(() => ({
-        transform: [{translateX: x.value}, {translateY: y.value}],
+        transform: [{translateX: x.value}, {translateY: y.value}, {scale: scale.value}, {perspective: 200}],
     }));
 
     return (
         <Container padding={0}>
             <PanGestureHandler onGestureEvent={handleDrag}>
-                <Box style={boxAnimatedStyle} />
+                <Animated.View>
+                    <PinchGestureHandler onGestureEvent={handlePinch}>
+                        <Box style={boxAnimatedStyle} />
+                    </PinchGestureHandler>
+                </Animated.View>
             </PanGestureHandler>
         </Container>
     );
